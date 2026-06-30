042948fb43ec31f261cd9100261d671e3f055250
odutt4440-cmyk <odutt4440@gmail.com>
2026-06-30 17:13:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -472,8 +472,7 @@ async def craft_handler(event):
 
 
 
-    recipe = await get_recipe(actual_item1, actual_item2)
-
+    recipe = await get_recipe(item1_input.capitalize(), item2_input.capitalize())
     
 
     if recipe:
